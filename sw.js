b836ec67037a03613d71b3982f4e160639475c3e
tmk3t <kikutadev@gmail.com>
2026-09-11 13:02:46 +0900

    fix: preserve github pages base path

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const RELEASE_ID = "2b05bd5f92cd";
+const RELEASE_ID = "d543884afbc2";
 const CACHE_NAMESPACE = `adventurer-guild-app:${self.registration.scope}`;
 const CACHE_NAME = `${CACHE_NAMESPACE}:${RELEASE_ID}`;
 const APP_SHELL_PATHS = [
@@ -29,8 +29,8 @@ const APP_SHELL_PATHS = [
   "assets/characters/generated/selene.png",
   "assets/characters/generated/tessa.png",
   "assets/dotgothic16-japanese-400-normal-DqORjlTR.woff2",
-  "assets/index-BpyIKBLH.css",
-  "assets/index-UHaNh1HJ.js",
+  "assets/index-CZGd1o2t.js",
+  "assets/index-DoRSD39g.css",
   "assets/vendor/fantasy-portraits/mage-placeholder.png",
   "assets/vendor/fantasy-portraits/scout.png",
   "assets/vendor/fantasy-portraits/warrior.png",
